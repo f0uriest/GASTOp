02a861d6cb4f15060d8c63100364c595cba745fe
dshaw150 <Dan@nat-oitwireless-inside-vapornet100-b-23809.Princeton.EDU>
2018-12-12 16:07:47 -0500

    Deleted the redundant testGenAlg_sfr and made new class inside test_GenAlg

--- test_GenAlg.py
+++ test_GenAlg.py
@@ -7,7 +7,8 @@ import numpy.testing as npt
 import GenAlg
 import Truss
 
-class TestGenerateRandom(unittest.TestCase):
+
+class TestGenAlg_Dan(unittest.TestCase):
     def test_(self):
 
         ga = GenAlg(ga_params,mutate_params,crossover_params,selector_params,
@@ -24,7 +25,7 @@ class TestGenerateRandom(unittest.TestCase):
 
 
 
-class TestGenAlg(unittest.TestCase):
+class TestGenAlg_Susan(unittest.TestCase):
 
     def testTruss(self):
 
@@ -48,3 +49,19 @@ class TestGenAlg(unittest.TestCase):
         #
         GA = GenAlg()#
         for current_gen in range(num_generations): # Loop over all generations:
+            self.progress_monitor(self.population,current_gen,ax1)
+            for current_truss in range(self.pop_size): # Loop over all trusses -> PARALLELIZE. Later
+                self.fitness_function(self.population[current_truss]) # Assigns numerical score to each truss
+            self.population = self.update_population(self.population) # Determine which members to
+        plt.show() #sfr, keep plot from closing right after this completes, terminal will hang until this is closed
+        return self.population[0], self.pop_progress
+
+        #GA = GenAlg()
+        #pop_test = GA.initialize_population(10)
+
+        fos = [i.fos for i in population] #extracts fos for each truss object in population
+
+
+        #note to susan: look up map() and filter()
+if __name__ == "__main__":
+    unittest.main()
