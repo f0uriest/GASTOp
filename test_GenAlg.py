ad585e3a1f721edecd2b3f75da582967a68205b4
dshaw150 <Dan@nat-oitwireless-inside-vapornet100-b-23809.Princeton.EDU>
2018-12-12 16:02:30 -0500

    Added more infrastructure in Test_GenAlg

--- test_GenAlg.py
+++ test_GenAlg.py
@@ -2,8 +2,10 @@
 
 import unittest
 import numpy as np
+import numpy.testing as npt
 
-import GenAlg as GA
+import GenAlg
+import Truss
 
 class TestGenerateRandom(unittest.TestCase):
     def test_(self):
@@ -14,5 +16,35 @@ class TestGenerateRandom(unittest.TestCase):
         ga.initialize_population(pop_size)
 
         for i in range(ga.pop_size):
-            for 
+            for
             self.assertTrue()
+
+
+
+
+
+
+class TestGenAlg(unittest.TestCase):
+
+    def testTruss(self):
+
+        nodes = np.array([[1,2,3],[2,3,4]])
+        edges = np.array([[0,1]])
+        properties = np.array([[0,3]])
+
+        pop_size = 10
+        population = [Truss.Truss(nodes,edges,properties) for i in range(pop_size)]
+
+        for truss in population:
+            truss.fos = random.random()
+
+        population.sort(key=lambda x: x.fos)
+                # print([x.fitness_score for x in population])
+        #dumb GA run
+        fig = plt.figure()
+        ax1 = fig.add_subplot(1,1,1)
+        plt.ylabel('fos')
+        plt.xlabel('iteration')
+        #
+        GA = GenAlg()#
+        for current_gen in range(num_generations): # Loop over all generations:
