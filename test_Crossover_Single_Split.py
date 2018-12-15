752fc3bfcf3a75951c333b09df6da1f7ce4c18fe
Rory Conlin <roryconlin@gmail.com>
2018-12-15 18:37:18 -0500

    renamed tests to comply with pytest syntax

--- test_Crossover_Single_Split.py
+++ test_Crossover_Single_Split.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import unittest
 import numpy as np
 
@@ -12,16 +14,20 @@ print('child_1 =\n',child_1)
 print('child_2 =\n',child_2)
 '''
 
+
 class TestCrossover_singlepointsplit(unittest.TestCase):
     def test_Basic(self):
         # Change point to 5 before running this test
-        array1 = np.arange(1,11,1).reshape((10,1))
-        array2 = np.arange(11,21,1).reshape((10,1))
+        array1 = np.arange(1, 11, 1).reshape((10, 1))
+        array2 = np.arange(11, 21, 1).reshape((10, 1))
         myCrossover = Crossover.Crossover([])
-        children = myCrossover.single_point_split(array1,array2)
+        children = myCrossover.single_point_split(array1, array2)
 
-        np.testing.assert_array_equal(children[0].T,[(1,2,3,4,5,16,17,18,19,20)])
-        np.testing.assert_array_equal(children[1].T,[(11,12,13,14,15,6,7,8,9,10)])
+        np.testing.assert_array_equal(
+            children[0].T, [(1, 2, 3, 4, 5, 16, 17, 18, 19, 20)])
+        np.testing.assert_array_equal(
+            children[1].T, [(11, 12, 13, 14, 15, 6, 7, 8, 9, 10)])
+
 
 if __name__ == '__main__':
     unittest.main()
