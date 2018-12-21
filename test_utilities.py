0a7ff085b05d3dacc9f03efed39ccd8ace0af084
Cristian Lacey <clacey@princeton.edu>
2018-12-21 15:06:08 -0500

    Added init_file_parser to utilities.py. Added corresponding test in test_utilities.py. Also, updated init.txt to accomodate new configobj library. New test passes.

--- test_utilities.py
+++ test_utilities.py
@@ -7,7 +7,18 @@ import utilities
 
 class TestUtilities_Cristian(unittest.TestCase): # Cristian
     def testInitFileParser(self):
-        pass
+        init_file_path = 'init.txt'
+        config = utilities.init_file_parser(init_file_path)
+        # for key in config:
+        #     print(key)
+        #     print(config[key])
+        self.assertTrue(type(config['ga_params']['num_elite']) is int)
+        self.assertTrue(type(config['ga_params']['percent_crossover']) is float)
+        self.assertTrue(type(config['mutate_params']['node_mutator_params']['boundaries'])\
+                        is type(np.array([1,1])))
+        self.assertTrue(type(config['mutate_params']['node_mutator_params']['int_flag'])\
+                        is bool)
+
 
 if __name__ == "__main__":
     unittest.main()
--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import configobj
+import ast
 
 def beam_file_parser(filename):
     """
@@ -50,8 +51,53 @@ def beam_file_parser(filename):
 
 
 def init_file_parser(init_file_path): # Cristian
-    # Gets all of the inputs from the file
-    pass
+    ''' Parse init file for input parameters.
+
+    Creates ConfigObj object, which reads input parameters as a nested
+    dictionary of strings. The string are then converted to their correct types
+    using the ConfigObj walk method and a transform function.
+
+    Args:
+        init_file_path (string): Path to the init file.
+
+    Returns:
+        config (ConfigObj object): Nested dicitonary of input parameters as
+            correct types.
+    '''
+    # Extract inputs from the file as strings
+    config = configobj.ConfigObj(init_file_path)
+
+    # Function used to convert each string in config to associated type
+    def transform(section,key):
+        val = section[key]
+        newval = val
+        # Convert string to float or int
+        try:
+            newval = float(val)
+            newval = int(val)
+        except ValueError:
+            pass
+        # Convert string to True, False, None
+        if val == 'True':
+            newval = True
+        elif val == 'False':
+            newval = False
+        elif val == 'None':
+            newval = None
+        # Convert string to numpy array
+        try:
+            a = ast.literal_eval(val)
+            if type(a) is list:
+                newval = np.array(a)
+        except:
+            pass
+        section[key] = newval
+
+    # Recursively walk through config object converting strings according to
+    # transform function.
+    config.walk(transform)
+
+    return config
 
 
 def cart2sph(x, y, z):
