199f581b0e9a3c802afb39e8152622ba3bb49a22
Rory Conlin <roryconlin@gmail.com>
2019-01-02 03:56:31 -0500

    added usage and installation instructions

--- docs/usage.rst
+++ docs/usage.rst
@@ -2,8 +2,55 @@
 Usage
 =====
 
-explain command line interface?
+Command Line
+************
 
-explain using it as library in script
+gastop can either be run from the command line, or used in a python script.
 
-example init file, etc.
+If it was installed from PyPI using pip, it can be run from the command line as:
+
+.. code-block:: bash
+
+	gastop <config_file_path>
+
+If the code was cloned from github instead, the normal commandline shortcut will not be installed.
+It can still be run from the repository main directory as:
+
+.. code-block:: bash
+
+	python -m gastop <config_file_path>
+
+In either case, additional arguments can be passed via the command line:
+
+.. code-block:: bash
+
+    	usage: gastop [-h] [-p] [-g] [-t] [-q | -d] config_path
+
+	positional arguments:
+	config_path          file path to gastop config file
+	
+	optional arguments:
+	-h, --help           show this help message and exit
+	-p , --pop_size      population size. If not specified, defaults to what is
+                             in config.
+	-g , --num_gens      number of generations. If not specified, defaults to
+                             what is in config.
+	-t , --num_threads   number of threads to use. If not specified, defaults to
+	                     what is in config.
+	-q, --quiet          hide progress display window
+	-d, --display        show progress display window
+
+
+Python Package
+**************
+
+gastop can also be used from within python, either in a script or interactively.
+
+.. code-block:: python
+		
+   import gastop
+   config_file_path = "./path_to_config_file.txt"
+   ga = gastop.GenAlg(config_file_path)
+   ga.initialize_population(pop_size=1e4)
+   best_truss, history = ga.run(num_generations=100, progress_display=1, num_threads=4)
+
--- gastop/progmon.py
+++ gastop/progmon.py
@@ -1,40 +1,39 @@
-
 import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy as np
-import Truss
-import Mutator
-import Crossover
-import Selector
-##plt.ion() #look into multithreading this
+from gastop import Truss, Mutator, Crossover, Selector
+
+# plt.ion() #look into multithreading this
 style.use('fivethirtyeight')
+
 
 class ProgMon():
 
-    def __init__(self,progress_display):
+    def __init__(self, progress_display):
 
         self.progress_display = progress_display
         self.pop_progress = []
 
-        if self.progress_display == 2: #check if figure method of progress monitoring is requested
+        if self.progress_display == 2:  # check if figure method of progress monitoring is requested
             # initialize plot:
             fig = plt.figure()
-            self.ax1 = fig.add_subplot(1,1,1) #does this need self.?
+            self.ax1 = fig.add_subplot(1, 1, 1)  # does this need self.?
             plt.ylabel('fos')
             plt.xlabel('iteration')
         #
 
-
-
-    def progress_monitor(self,current_gen,population):
+    def progress_monitor(self, current_gen, population):
 
         # three options: plot, progress bar ish thing, no output just append
         # calc population diversity and plot stuff or show current results
-        fitscore = [i.fitness_score for i in self.population] #extract factor of safety from each truss object in population
-        self.pop_progress.append(population) #append to history
+        # extract factor of safety from each truss object in population
+        fitscore = [i.fitness_score for i in self.population]
+        self.pop_progress.append(population)  # append to history
         if self.progress_display == 1:
-            print(current_gen,min(fitscore))
+            print(current_gen, min(fitscore))
         elif self.progress_display == 2:
-            #print(current_gen,min(fitscore))
-            self.ax1.scatter(current_gen,min(fitscore),c=[0,0,0]) #plot minimum fitscore for current gen in black
-            plt.pause(0.0001) #pause for 0.0001s to allow plot to update, can potentially remove this
+            # print(current_gen,min(fitscore))
+            # plot minimum fitscore for current gen in black
+            self.ax1.scatter(current_gen, min(fitscore), c=[0, 0, 0])
+            # pause for 0.0001s to allow plot to update, can potentially remove this
+            plt.pause(0.0001)
