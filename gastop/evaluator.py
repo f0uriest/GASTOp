fixed coordinate transformation bug in eval and singular rotation thing

gastop/evaluator.py
@@ -168,6 +168,8 @@ class Evaluator():
         loads = boundary_conditions['loads'].copy()
         fixtures = boundary_conditions['fixtures'].copy()
 
+        eps = np.finfo(float).eps  # machine precision
+
         num_nodes = nodes.shape[0]
         num_con = con.shape[0]
         num_loads = loads.shape[2]
@@ -202,14 +204,18 @@ class Evaluator():
 
         # calculate length and direction sines/cosines of members in global coords
         edge_vec = nodes[con[:, 1], :] - nodes[con[:, 0], :]
-        rho = np.sqrt(edge_vec[:, 0]**2+edge_vec[:, 1]
-                      ** 2)  # length in x-y plane
-        L = np.sqrt(rho**2 + edge_vec[:, 2]**2)  # total length of member
-# fix singular azimuthal angle
+        # length of projection in x-y plane
+        rho = np.sqrt(edge_vec[:, 0]**2+edge_vec[:, 2]**2)
+        L = np.sqrt(rho**2 + edge_vec[:, 1]**2)  # total length
+        sing = rho < eps  # id vectors along z axis, azimuth is not defined, convention set to 0
+        edge_vec[:, 0][sing] = 1  # cos(0) = 1
+        edge_vec[:, 2][sing] = 0  # sin(0) = 0
+        rho[sing] = 1
         ca = edge_vec[:, 0]/rho  # cosine of azimuthal angle
-        sa = edge_vec[:, 1]/rho  # sine of azimuthal angle
-        cp = rho/L  # cosine of polar angle
-        sp = edge_vec[:, 2]/L  # sine of polar angle
+        sa = edge_vec[:, 2]/rho  # sine of azimuthal angle
+        rho[sing] = 0
+        cp = rho/L  # cosine of polar elevation angle
+        sp = edge_vec[:, 1]/L  # sine of polar elevation angle
 
         # populate transformation matrices
         r[0, 0, :] = cp*ca

profiling/angles.py
@@ -22,8 +22,8 @@ def cart2sph(x, y, z):
     z = np.array(z)
 
     r = np.sqrt(x**2+y**2+z**2)
-    azim = np.arctan2(y, x)
-    elev = np.pi/2 - np.arccos(z/r)
+    azim = np.arctan2(z, x)
+    elev = np.pi/2 - np.arccos(y/r)
 
     return r, elev, azim
 
@@ -39,15 +39,23 @@ def method1(edge_vec):
 
 
 def method2(edge_vec):
-    rho = np.sqrt(edge_vec[:, 0]**2+edge_vec[:, 1]**2)
-    L = np.sqrt(rho**2 + edge_vec[:, 2]**2)
+    eps = 1e-16
+    rho = np.sqrt(edge_vec[:, 0]**2+edge_vec[:, 2]**2)
+    L = np.sqrt(rho**2 + edge_vec[:, 1]**2)
+    sing = rho < eps
+    edge_vec[:, 0][sing] = 1
+    edge_vec[:, 2][sing] = 0
+    rho[sing] = 1
     ca = edge_vec[:, 0]/rho
-    sa = edge_vec[:, 1]/rho
+    sa = edge_vec[:, 2]/rho
+    rho[sing] = 0
     cb = rho/L
-    sb = edge_vec[:, 2]/L
+    sb = edge_vec[:, 1]/L
     return ca, sa, cb, sb
 
 
+cas1, sas1, cbs1, sbs1 = method1(np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]]))
+cas2, sas2, cbs2, sbs2 = method2(np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]]))
 ca1, sa1, cb1, sb1 = method1(edge_vec)
 ca2, sa2, cb2, sb2 = method2(edge_vec)
 
@@ -56,6 +64,8 @@ np.testing.assert_array_almost_equal(ca1, ca2)
 np.testing.assert_array_almost_equal(sa1, sa2)
 np.testing.assert_array_almost_equal(cb1, cb2)
 np.testing.assert_array_almost_equal(sb1, sb2)
+np.testing.assert_array_almost_equal(
+    [cas1, sas1, cbs1, sbs1], [cas2, sas2, cbs2, sbs2])
 
 test1 = '''ca1,sa1,cb1,sb1 = method1(edge_vec)'''
 test2 = '''ca2,sa2,cb2,sb2 = method2(edge_vec)'''

tests/test_eval.py
@@ -16,12 +16,47 @@ class TestEvaluator(unittest.TestCase):
     mass solver, and cost solver.
     """
 
-    def test_axial_load(self):
+    def test_axial_load_y(self):
         """Tests simple straight beam under axial forces"""
 
         p = 1000  # load in newtons
         L = 2  # length in meters
         matl = 0
+        rand_nodes = np.array([]).reshape(0, 3)  # no random nodes
+        user_spec_nodes = np.array([[0, 0, 0], [0, L, 0]])
+        edges = np.array([[0, 1]])
+        properties = np.array([matl])
+        truss = Truss(user_spec_nodes, rand_nodes, edges, properties)
+        dof = np.array([[1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]
+                       ).reshape(2, 6, 1)
+        load = np.array(
+            [[0, 0, 0, 0, 0, 0], [0, p, 0, 0, 0, 0]]).reshape(2, 6, 1)
+        beam_dict = utilities.beam_file_parser('gastop-config/properties.csv')
+        bdry = {'loads': load, 'fixtures': dof}
+        evaluator = Evaluator('mat_struct_analysis_DSM',
+                              'mass_basic', 'interference_ray_tracing', 'cost_calc', bdry, beam_dict)
+        evaluator(truss)
+        A = beam_dict['x_section_area'][matl]
+        E = beam_dict['elastic_modulus'][matl]
+        sigma = p/A
+        fos_true = beam_dict['yield_strength'][matl]/sigma
+        strain = sigma/E
+        deflection_true = np.array([[[0], [0], [0], [0], [0], [0]],
+                                    [[0], [strain*L], [0], [0], [0], [0]]])
+
+        np.testing.assert_almost_equal(
+            truss.mass, A*L*beam_dict['density'][matl])
+        np.testing.assert_array_almost_equal(truss.fos, fos_true)
+        np.testing.assert_array_almost_equal(truss.deflection, deflection_true)
+        self.assertTrue(truss.interference is None)
+        np.testing.assert_almost_equal(truss.cost, L*beam_dict['cost'][matl])
+
+    def test_axial_load_x(self):
+        """Tests simple straight beam under axial forces"""
+
+        p = 10000  # load in newtons
+        L = .5  # length in meters
+        matl = 4
         rand_nodes = np.array([]).reshape(0, 3)  # no random nodes
         user_spec_nodes = np.array([[0, 0, 0], [L, 0, 0]])
         edges = np.array([[0, 1]])
@@ -48,8 +83,39 @@ class TestEvaluator(unittest.TestCase):
             truss.mass, A*L*beam_dict['density'][matl])
         np.testing.assert_array_almost_equal(truss.fos, fos_true)
         np.testing.assert_array_almost_equal(truss.deflection, deflection_true)
-        self.assertTrue(truss.interference is None)
-        np.testing.assert_almost_equal(truss.cost, L*beam_dict['cost'][matl])
+
+    def test_axial_load_z(self):
+        """Tests simple straight beam under axial forces"""
+
+        p = 5000  # load in newtons
+        L = 4  # length in meters
+        matl = 1
+        rand_nodes = np.array([]).reshape(0, 3)  # no random nodes
+        user_spec_nodes = np.array([[0, 0, 0], [0, 0, L]])
+        edges = np.array([[0, 1]])
+        properties = np.array([matl])
+        truss = Truss(user_spec_nodes, rand_nodes, edges, properties)
+        dof = np.array([[1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]
+                       ).reshape(2, 6, 1)
+        load = np.array(
+            [[0, 0, 0, 0, 0, 0], [0, 0, p, 0, 0, 0]]).reshape(2, 6, 1)
+        beam_dict = utilities.beam_file_parser('gastop-config/properties.csv')
+        bdry = {'loads': load, 'fixtures': dof}
+        evaluator = Evaluator('mat_struct_analysis_DSM',
+                              'mass_basic', 'interference_ray_tracing', 'cost_calc', bdry, beam_dict)
+        evaluator(truss)
+        A = beam_dict['x_section_area'][matl]
+        E = beam_dict['elastic_modulus'][matl]
+        sigma = p/A
+        fos_true = beam_dict['yield_strength'][matl]/sigma
+        strain = sigma/E
+        deflection_true = np.array([[[0], [0], [0], [0], [0], [0]],
+                                    [[0], [0], [strain*L], [0], [0], [0]]])
+
+        np.testing.assert_almost_equal(
+            truss.mass, A*L*beam_dict['density'][matl])
+        np.testing.assert_array_almost_equal(truss.fos, fos_true)
+        np.testing.assert_array_almost_equal(truss.deflection, deflection_true)
 
     def test_singular_stiffness_matrix(self):
         """Tests straight beam under axial forces with no restraints