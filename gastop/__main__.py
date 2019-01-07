f2146fa3b9f34858d215b89aad08824485204cfb
Rory Conlin <roryconlin@gmail.com>
2019-01-07 13:51:20 -0500

    removed animation plotting from __main__.py

--- gastop/__main__.py
+++ gastop/__main__.py
@@ -96,15 +96,6 @@ def main(args=sys.argv[1:]):
                   fixtures=config['evaluator_params']['boundary_conditions']['fixtures'],
                   deflection=True)
 
-    if progress_truss and not progress_fitness:
-        images = []
-        for i in range(num_generations):
-            images.append(imageio.imread(
-                'animation/truss_evo_iter' + str(i) + '.png'))
-        imageio.mimsave('animation/truss_evo_gif.gif', images, duration=0.5)
-
-#utilities.save_gif(progress_history, progress_fitness, progress_truss, animation_path, num_gens,config)
-
 
 if __name__ == '__main__':
     main(sys.argv[1:])
